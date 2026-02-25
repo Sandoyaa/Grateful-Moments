7e1c8090a8b16b2b399367ea67367f061c36aa2a
sandoya <sergeysurzhikov2@gmail.com>
2026-02-25 18:28:02 +0300

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Siyanie on 25.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
